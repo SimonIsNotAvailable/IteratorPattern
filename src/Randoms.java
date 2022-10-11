8b376366950371a4c0d38a8fbe418ea4e50bf4db
SimonIsNotAvailable <polushkinsn@gmail.com>
2022-10-11 17:59:00 +0500

    private access for vars added

--- src/Randoms.java
+++ src/Randoms.java
@@ -2,8 +2,8 @@ import java.util.Iterator;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
-    protected Random random;
-    protected int min, max;
+    private Random random;
+    private int min, max;
 
     public Randoms(int min, int max) {
         this.random = new Random();
